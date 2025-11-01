e812fcf264b56f4a75a131a5bff00f58f452ed48
ashve <yuliashvetsova1981@gmail.com>
2025-11-01 17:05:05 -0500

    New code 3

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = ' https://cnt-d03fe6e0-ca3e-4046-a077-a0acf94975c7.containerhub.tripleten-services.com'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
